6579c33da8b9e654f1857e9fb86e84a13ab67f24
Tyler <tbuchhei@alumni.nd.edu>
2018-01-02 16:16:26 -0600

    Set up landing and index routes

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+const campgrounds = [
+  {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+  {name: "Granite Hill", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8577/16263386718_c019b13f77.jpg"},
+]
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  res.render("campgrounds", {campgrounds})
+});
+
+app.listen(3000, () => console.log("YelpCamp server running on port 3000"));
