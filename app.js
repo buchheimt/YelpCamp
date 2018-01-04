d0ecf2e63667c1c618f2b431fa0c8c13cb851e32
Tyler <tbuchhei@alumni.nd.edu>
2018-01-03 23:59:29 -0600

    Implement comment#new and comment#create

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express     = require("express"),
       bodyParser  = require("body-parser"),
       mongoose    = require("mongoose"),
       Campground  = require("./models/campground"),
+      Comment     = require("./models/comment"),
       seedDB      = require("./seeds");
 
-
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+seedDB();
 
 app.get("/", (req, res) => {
   res.render("landing");
@@ -20,7 +20,7 @@ app.get("/campgrounds", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", {campgrounds});
+      res.render("campgrounds/index", {campgrounds});
     }
   });
 });
@@ -41,7 +41,7 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new.ejs");
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", (req, res) => {
@@ -49,10 +49,43 @@ app.get("/campgrounds/:id", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(campground);
-      res.render("show", {campground});
+      res.render("campgrounds/show", {campground});
     }
   });
 });
+
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("adding comment to campground")
+          console.log(campground);
+          console.log(comment);
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+});
+
 
 app.listen(3000, () => console.log("YelpCamp server running on port 3000"));
