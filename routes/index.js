398ec05e39171d62025a8310ed66b16db928d95b
Tyler <tbuchhei@alumni.nd.edu>
2018-01-07 10:12:13 -0600

    Add flash messages

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,12 @@ router.post("/register", (req, res) => {
   const newUser = new User({username: req.body.username});
   User.register(newUser, req.body.password, (err, user) => {
     if (err) {
-      console.log(err);
-      return res.render("register");
+      req.flash("error", err.message);
+      return res.redirect("/register");
     }
+
     passport.authenticate("local")(req, res, () => {
+      req.flash("success", "Welcome to YelpCamp " + user.username);
       res.redirect("/campgrounds");
     });
   });
@@ -44,6 +46,7 @@ router.post("/login", passport.authenticate("local",
 // session#destroy
 router.get("/logout", (req, res) => {
   req.logout();
+  req.flash("success", "Logged you out!");
   res.redirect("/campgrounds");
 });
 
