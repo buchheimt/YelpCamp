4fbc16e00e4f80fa9bd3acda1a8c6cd07500bd11
Tyler <tbuchhei@alumni.nd.edu>
2018-01-06 18:36:07 -0600

    Implement campground#edit and campground#update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express       = require("express"),
-      app           = express(),
-      bodyParser    = require("body-parser"),
-      mongoose      = require("mongoose"),
-      passport      = require("passport"),
-      LocalStrategy = require("passport-local"),
-      Campground    = require("./models/campground"),
-      Comment       = require("./models/comment"),
-      User          = require("./models/user"),
-      seedDB        = require("./seeds");
+const express        = require("express"),
+      app            = express(),
+      bodyParser     = require("body-parser"),
+      mongoose       = require("mongoose"),
+      passport       = require("passport"),
+      LocalStrategy  = require("passport-local"),
+      methodOverride = require("method-override"),
+      Campground     = require("./models/campground"),
+      Comment        = require("./models/comment"),
+      User           = require("./models/user"),
+      seedDB         = require("./seeds");
 
 const campgroundRoutes = require("./routes/campgrounds"),
       commentRoutes = require("./routes/comments"),
@@ -17,6 +18,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 //seedDB();
 
 app.use(require("express-session")({
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,6 +49,28 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// campground#edit
+router.get("/:id/edit", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/edit", {campground});
+    }
+  })
+});
+
+// campground#update
+router.put("/:id", (req, res) => {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds/" + campground._id);
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
