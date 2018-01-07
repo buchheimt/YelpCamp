33a38965b3606b6e4234236648d232185357f4df
Tyler <tbuchhei@alumni.nd.edu>
2018-01-07 15:37:14 -0600

    Fix port issue

--- app.js
+++ app.js
@@ -46,4 +46,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => console.log("YelpCamp server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("YelpCamp server running on port 3000"));
